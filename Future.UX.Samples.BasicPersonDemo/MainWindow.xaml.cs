d2b8428327c731ff1eb6bd7501e19af7e4c209ae
Rich Harrison <r.harrison891@gmail.com>
2025-11-14 19:28:06 +0000

    Added Basic Person Demo project

---
+++ Future.UX.Samples.BasicPersonDemo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Future.UX.Samples.BasicPersonDemo
+{
+    public partial class MainWindow : Window
+    {
+        public Person Person { get; } = new Person() { FirstName = "John", LastName = "Smith" };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Future.UX.Samples.BasicPersonDemo/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Future.UX.Samples.BasicPersonDemo
+{
+    public partial class Person
+    {
+        //Simple setup for fields
+        private string? __firstName;
+        private string? __lastName;
+        private bool __buttonEnabled = true;
+        private int __clickCount;
+
+        //Commands
+        private void __ClickIncrementer(object parameter) => ClickCount += Convert.ToInt32(parameter);
+        private bool __ClickIncrementer_Can(object parameter) => ButtonEnabled;
+
+        //Auto notifying computed properties
+        public string FullName => $"{FirstName} {LastName}";
+        public string ClickCountText => $"Clicked {ClickCount} times";
+    }
+}
